7c0ba7ad72251ceb579fec9d6ee3b0138b1581c9
jorgehernand <jorgehernand@gmail.com>
2021-08-24 17:14:51 +0200

    Added random Date step1

--- mgp.js
+++ mgp.js
@@ -1,14 +1,20 @@
 let mgp = {};
 
+
+
 mgp.generate = function () {
   
   //randomize staff
 
-  //take data from somewhere
+  //take data from somewhere with this random thing
 
   //present result as String
   console.log("Random generated String from somewhere ");
   return "";
+};
+
+mgp.randomDate = (start, end) => {
+  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 };
 
 
--- mgp.test.js
+++ mgp.test.js
@@ -1,6 +1,14 @@
 const mgpTest = require("./mgp");
 
 
-test("Test the function from obj is callable", () => {
-  expect(mgpTest.generate()).toBe("");
+
+test("Test the function from obj is a String ", () => {
+  const value = mgpTest.generate();
+  expect(typeof value).toBe('string');  
+});
+
+
+test("Print random date ", () => {
+  console.log(mgpTest.randomDate(new Date(2012, 0, 1), new Date()));
+  //expect(typeof value).toBe('string');  
 });
